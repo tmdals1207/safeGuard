7d15899719f3a404817bbe40012d7373d684d3a1
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2024-03-19 17:32:21 +0900

    Initialize Project (#1)

---
+++ src/test/java/com/capstone/safeGuard/SafeGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.safeGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
