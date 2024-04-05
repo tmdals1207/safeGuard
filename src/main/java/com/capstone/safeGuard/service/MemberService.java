c25ab93db576acf577123c54657502868aec4704
LeeShinHaeng <dragon117@kyonggi.ac.kr>
2024-04-05 15:25:31 +0900

    [feat] login, signup에 암호화 이용

--- src/main/java/com/capstone/safeGuard/service/MemberService.java
+++ src/main/java/com/capstone/safeGuard/service/MemberService.java
@@ -1,39 +1,40 @@
 package com.capstone.safeGuard.service;
 
+import com.capstone.safeGuard.domain.Child;
 import com.capstone.safeGuard.domain.Member;
 import com.capstone.safeGuard.dto.request.LoginRequestDTO;
 import com.capstone.safeGuard.dto.request.SignUpRequestDTO;
 import com.capstone.safeGuard.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public Boolean login(LoginRequestDTO dto) {
-        if(dto.getLoginType().equals(LoginType.Member.toString())){
-            return memberLogin(dto);
-        }else{
-            return childLogin(dto);
-        }
-    }
-
-    private boolean memberLogin(LoginRequestDTO dto) {
+    public Member memberLogin(LoginRequestDTO dto) {
         Optional<Member> findMember = memberRepository.findById(dto.getEditTextID());
         if(findMember.isEmpty()){
-            return true;
+            return null;
         }
 
-        return findMember.get()
-                .getPassword()
-                .equals(dto.getEditTextPW());
+        return findMemberWithAuthenticate(findMember, dto.getEditTextPW());
     }
 
-    private Boolean childLogin(LoginRequestDTO dto) {
+    private Member findMemberWithAuthenticate(Optional<Member> findMember, String rawPassword){
+        return findMember
+                .filter(member -> passwordEncoder.matches(rawPassword, member.getPassword()))
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public Child childLogin(LoginRequestDTO dto) {
         return null;
     }
 
@@ -47,7 +48,8 @@ public class MemberService {
         member.setMemberId(dto.getInputId());
         member.setEmail(dto.getInputEmail());
         member.setName(dto.getInputName());
-        member.setPassword(dto.getInputPW());
+        String encodedPassword = passwordEncoder.encode(dto.getInputPW());
+        member.setPassword(encodedPassword);
         memberRepository.save(member);
 
         return true;
