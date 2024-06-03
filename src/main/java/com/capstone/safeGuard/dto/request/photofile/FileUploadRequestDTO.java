ce15eac2c5eaa567b0a456ee2727c3b83799b37b
LeeShinHaeng <dragon117@kyonggi.ac.kr>
2024-06-03 19:33:56 +0900

    [refactor] unused import 제거

--- src/main/java/com/capstone/safeGuard/dto/request/photofile/FileUploadRequestDTO.java
+++ src/main/java/com/capstone/safeGuard/dto/request/photofile/FileUploadRequestDTO.java
@@ -1,7 +1,6 @@
 package com.capstone.safeGuard.dto.request.photofile;
 
 import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 public class FileUploadRequestDTO {
